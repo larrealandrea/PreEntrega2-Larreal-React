722ea2246104a54594da4eb419f606861a170f21
andrea larreal <larrealandrea3@gmail.com>
2024-01-08 18:25:24 -0300

    modificando firebase

--- src/firebase/client.js
+++ src/firebase/client.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app"
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID
+  apiKey: "AIzaSyADsRCdq_MCbJiaGhJFbffjqgxDGNdhQOs",
+  authDomain: "ecommece-coder.firebaseapp.com",
+  projectId: "ecommece-coder",
+  storageBucket: "ecommece-coder.appspot.com",
+  messagingSenderId: "89225874128",
+  appId: "1:89225874128:web:285bb2947a7d6d13286cd5"
 };
 
 const app = initializeApp(firebaseConfig);
